b73f9b023fe748652c2a956ea2f6a03febc33c03
ngbricen <bricen@gmail.com>
2017-06-14 00:21:48 -0400

    Solution working with no messaging or styling - other bugs resolved

--- assets/javascript/rocklogic.js
+++ assets/javascript/rocklogic.js
@@ -35,72 +35,76 @@ $("#turnNameMessage").hide();
 // At the initial load and subsequent value changes, get a snapshot of the local data.
 // This function allows you to update your page in real-time when the firebase database changes.
 database.ref().on("value", function(snapshot) {
-
-  	// If Firebase has a player stored (first player)
-	if (snapshot.child("players/1").exists() ) {
-		user1Name = snapshot.val().players[1].name;
-		user1Choice = snapshot.val().players[1].choice;
-		user1Win = snapshot.val().players[1].wins;
-		user1Loss = snapshot.val().players[1].losses;
-		user1Draw = snapshot.val().players[1].draws;
-
-		$("#user1Title").text(user1Name);
-		$("#user1Score").text("Wins: " + user1Win + " Losses: " + user1Loss + " Draws: " + user1Draw);
+	if (snapshot.val().turn === 0 ) {
+		$("#turnNameMessage").empty();
 	}
-	else {
-		$("#user1Title").text("Waiting For Player 1");  	
-	}
+	else
+	{
+	  	// If Firebase has a player stored (first player)
+		if (snapshot.child("players/1").exists() ) {
+			user1Name = snapshot.val().players[1].name;
+			user1Choice = snapshot.val().players[1].choice;
+			user1Win = snapshot.val().players[1].wins;
+			user1Loss = snapshot.val().players[1].losses;
+			user1Draw = snapshot.val().players[1].draws;
 
-	if (snapshot.child("turn").exists() ) {
-		turnCount = snapshot.val().turn;
-		sessionStorage.setItem("turnCount", turnCount);
-	}
+			$("#user1Title").text(user1Name);
+			$("#user1Score").text("Wins: " + user1Win + " Losses: " + user1Loss + " Draws: " + user1Draw);
+		}
+		else {
+			$("#user1Title").text("Waiting For Player 1");  	
+		}
 
-	if (snapshot.child("players/2").exists() ) {
-		user2Name = snapshot.val().players[2].name;
-		user2Choice = snapshot.val().players[2].choice;
-		user2Win = snapshot.val().players[2].wins;
-		user2Loss = snapshot.val().players[2].losses;
-		user2Draw = snapshot.val().players[2].draws;
+		if (snapshot.child("turn").exists() ) {
+			turnCount = snapshot.val().turn;
+			sessionStorage.setItem("turnCount", turnCount);
+		}
 
-		$("#user2Title").text(user2Name);
+		if (snapshot.child("players/2").exists() ) {
+			user2Name = snapshot.val().players[2].name;
+			user2Choice = snapshot.val().players[2].choice;
+			user2Win = snapshot.val().players[2].wins;
+			user2Loss = snapshot.val().players[2].losses;
+			user2Draw = snapshot.val().players[2].draws;
 
-		$("#turnNameMessage").show();
-		$("#turnNameMessage").text("Waiting for " + user1Name + " to choose!");
-		
-		$("#user2Score").text("Wins: " + user2Win + " Losses: " + user2Loss + " Draws: " + user2Draw);
-		
-		if (sessionStorage.getItem("currentUserName") === user1Name){		
-			if (turnCount === 1){
+			$("#user2Title").text(user2Name);
+
+			$("#turnNameMessage").show();
+			$("#turnNameMessage").text("Waiting for " + user1Name + " to choose!");
+			
+			$("#user2Score").text("Wins: " + user2Win + " Losses: " + user2Loss + " Draws: " + user2Draw);
+			
+			if (sessionStorage.getItem("currentUserName") === user1Name){		
+				if (turnCount === 1){
+					$("#turnNameMessage").text("It's Your Turn!");
+					$("#user1Options").empty();
+					$("#user1Options").append($("<h3><span class='user1choices'>Rock</span></h3>"));
+					$("#user1Options").append($("<h3><span class='user1choices'>Paper</span></h3>"));
+					$("#user1Options").append($("<h3><span class='user1choices'>Scissors</span></h3>"));			
+				}
+				else{
+					$("#turnNameMessage").text("Waiting for " + user2Name + " to choose!");		
+				}
+			}
+
+			if (sessionStorage.getItem("currentUserName") !== user1Name && turnCount === 2){
 				$("#turnNameMessage").text("It's Your Turn!");
-				$("#user1Options").empty();
-				$("#user1Options").append($("<h3><span class='user1choices'>Rock</span></h3>"));
-				$("#user1Options").append($("<h3><span class='user1choices'>Paper</span></h3>"));
-				$("#user1Options").append($("<h3><span class='user1choices'>Scissors</span></h3>"));			
+
+				$("#user2Options").append($("<h3><span class='user2choices'>Rock</span></h3>"));
+				$("#user2Options").append($("<h3><span class='user2choices'>Paper</span></h3>"));
+				$("#user2Options").append($("<h3><span class='user2choices'>Scissors</span></h3>"));			
 			}
-			else{
-				$("#turnNameMessage").text("Waiting for " + user2Name + " to choose!");		
-			}
+		
+	  	}
+	  	else {
+
+	  		$("#user2Title").text("Waiting For Player 2");
+	  	}
+
+		if (snapshot.child("players/1/choice").exists() && snapshot.child("players/2/choice").exists() && turnCount === 3){
+			displayResults(user1Choice,user2Choice,user1Win,user1Loss,user1Draw, user2Win, user2Loss, user2Draw);
 		}
-
-		if (sessionStorage.getItem("currentUserName") !== user1Name && turnCount === 2){
-			$("#turnNameMessage").text("It's Your Turn!");
-
-			$("#user2Options").append($("<h3><span class='user2choices'>Rock</span></h3>"));
-			$("#user2Options").append($("<h3><span class='user2choices'>Paper</span></h3>"));
-			$("#user2Options").append($("<h3><span class='user2choices'>Scissors</span></h3>"));			
-		}
-	
-  	}
-  	else {
-
-  		$("#user2Title").text("Waiting For Player 2");
-  	}
-
-	if (snapshot.child("players/1/choice").exists() && snapshot.child("players/2/choice").exists() && turnCount === 3){
-		displayResults(user1Choice,user2Choice,user1Win,user1Loss,user1Draw, user2Win, user2Loss, user2Draw);
 	}
-
   // If any errors are experienced, log them to console.
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -205,7 +209,7 @@ $(document).on("click",".user2choices",function(){
 });
 
 function displayResults(a,b,w1,l1,d1,w2,l2,d2){
-		$("#turnNameMessage").hide();
+		$("#turnNameMessage").empty();
 
 		$("#user1Options").empty();
 		$("#user2Options").empty();
@@ -222,12 +226,12 @@ function displayResults(a,b,w1,l1,d1,w2,l2,d2){
 		if ((a === "Rock" && b === "Scissors") || (a === "Scissors" && b === "Paper") || (a === "Paper" && b === "Rock")  ){
 			w1++;
 			l2++;
-			$("#results").append("<h1>" + user1Name + " Wins </h1>");
+			$("#results").append("<h1>" + user1Name + " WINS </h1>");
 		}
 		else {
 			l1++;
 			w2++;
-			$("#results").append("<h1>" + user2Name + " Wins </h1>");	
+			$("#results").append("<h1>" + user2Name + " WINS </h1>");	
 		}
 	}
 
@@ -238,7 +242,7 @@ function displayResults(a,b,w1,l1,d1,w2,l2,d2){
 		"players/2/wins": w2,
 		"players/2/losses": l2,
 		"players/2/draws": d2,
-		turn: 1
+		turn: 0
 	});
 		
 	$("#user1Score").text("Wins: " + w1 + " Losses: " + l1 + " Draws: " + d1);
@@ -250,15 +254,16 @@ function displayResults(a,b,w1,l1,d1,w2,l2,d2){
 		$("#user2Options").empty();
 
 		//Re-Display User 1 Options
-	if (sessionStorage.getItem("currentUserName") === user1Name){		
-		$("#user1Options").append($("<h3><span class='user1choices'>Rock</span></h3>"));
-		$("#user1Options").append($("<h3><span class='user1choices'>Paper</span></h3>"));
-		$("#user1Options").append($("<h3><span class='user1choices'>Scissors</span></h3>"));			
-	}
+		if (sessionStorage.getItem("currentUserName") === user1Name){		
+			$("#user1Options").append($("<h3><span class='user1choices'>Rock</span></h3>"));
+			$("#user1Options").append($("<h3><span class='user1choices'>Paper</span></h3>"));
+			$("#user1Options").append($("<h3><span class='user1choices'>Scissors</span></h3>"));			
+		}
+
+		database.ref().update({
+			turn: 1
+		});		
 	}, 3000);
 	
 
-}
-
-function fadeOutResults() {
 }
